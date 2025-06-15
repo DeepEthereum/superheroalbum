737a2b243f1ad38709dc1ff4d9de4ab06b01e360
yoser <yoser.giannetto@studenti.unimi.it>
2025-06-15 03:20:05 +0200

    add some file

--- JS/signup.js
+++ JS/signup.js
@@ -3,6 +3,7 @@ const form = document.querySelector(".needs-validation");
 form.addEventListener("submit", (e) => {
   if (!form.checkValidity()) {
     e.preventDefault();
+    e.stopPropagation()
   } else {
     alert("Success");
   }
